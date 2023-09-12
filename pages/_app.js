a8db7597a3713837a2929ce7e481fb50e53c8bca
Baxter <davidf.martinez.br@gmail.com>
2023-09-12 17:17:17 -0500

    Setup app page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,32 @@
-import '@/styles/globals.css'
+import Head from 'next/head';
+
+//css
+import '@/src/styles/globals.css';
+
+//components
+import Header from '../components/Header';
+
+//fonts
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({
+	subsets: ['latin'],
+	variable: '--font-mont',
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<main
+				className={`${montserrat.variable} font-sans bg-light w-full min-h-screen`}
+			>
+				<Header />
+				<Component {...pageProps} />
+			</main>
+		</>
+	);
 }
