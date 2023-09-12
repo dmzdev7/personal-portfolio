4d6849d317340d00c76241ef02290e1462f0d566
Baxter <davidf.martinez.br@gmail.com>
2023-09-12 17:04:50 -0500

    Added NavBar functionality

---
+++ components/NavBar.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Link from 'next/link';
+import { Router, useRouter } from 'next/router';
+
+//icons
+import {
+	HiHome,
+	HiUser,
+	HiViewColumns,
+	HiRectangleGroup,
+	// HiChatBubbleBottomCenterText,
+	HiEnvelope,
+} from 'react-icons/hi2';
+
+//navData
+export const navData = [
+	{ name: 'Home', path: '/', icon: <HiHome /> },
+	{ name: 'About', path: '/about', icon: <HiUser /> },
+	{ name: 'Services', path: '/services', icon: <HiRectangleGroup /> },
+	{ name: 'Projects', path: '/projects', icon: <HiViewColumns /> },
+	{ name: 'Contact', path: '/contact', icon: <HiEnvelope /> },
+];
+
+const NavBar = () => {
+	const router = useRouter();
+	const pathName = router.pathname;
+
+	return (
+		<nav className="flex items-center xl:justify-center gap-x-4">
+			{navData.map((link, index) => {
+				return (
+					<Link
+						key={index}
+						href={link.path}
+						className={`${
+							link.path === pathName && 'text-redEngine'
+						} hover:text-redEngine`}
+					>
+						<div className="flex items-center xl:justify-center gap-x-2">
+							{link.icon}
+							{link.name}
+						</div>
+					</Link>
+				);
+			})}
+		</nav>
+	);
+};
+
+export default NavBar;
