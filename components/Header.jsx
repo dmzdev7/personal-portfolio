8245c32225f7277c21d2ee9c7febe403773cf763
Baxter <davidf.martinez.br@gmail.com>
2023-09-12 17:05:51 -0500

    Added Header functionality

---
+++ components/Header.jsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import NavBar from './NavBar';
+
+const Header = () => {
+	return (
+		<header className="w-full px-32 py-6 font-medium flex justify-between items-center">
+			<Link href={'/'}>
+				<Image
+					src={'/imgs/svgs/logo.svg'}
+					width={220}
+					height={48}
+					alt="Dmzdev"
+					priority={true}
+				/>
+			</Link>
+			<NavBar />
+		</header>
+	);
+};
+
+export default Header;
